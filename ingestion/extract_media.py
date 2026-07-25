4a4a06c5c6cd6844ffb51bce0d1ae5768de341a9
cntaylortrace316 <cntaylor316@gmail.com>
2026-07-25 06:55:32 -0600

    Refactor get_media to fetch media stats from API

--- ingestion/extract_media.py
+++ ingestion/extract_media.py
@@ -1,5 +1,16 @@
-def get_media():
-    return {
-        "media_id": "8hunphufxp",
-        "title": "Sample Video"
+import requests
+
+
+def get_media(media_id, token):
+
+    url = f"https://api.wistia.com/v1/stats/medias/{media_id}.json"
+
+    headers = {
+        "Authorization": f"Bearer {token}"
     }
+
+    response = requests.get(url, headers=headers)
+
+    response.raise_for_status()
+
+    return response.json()
